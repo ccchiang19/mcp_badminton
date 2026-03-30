4ab4d857b9e6ccb5d61b4062c8f4478e242026ca
Chien <capricornusonce@gmail.com>
2026-03-30 16:30:25 +0800

    v1

--- server.py
+++ server.py
@@ -6,5 +6,11 @@ mcp = FastMCP("My MCP Server")
 def greet(name: str) -> str:
     return f"Hello, {name}!"
 
+@mcp.tool
+def score_evaluation(score: int) -> str:
+    if score > 60:
+        return f"Good jobbbbbbbbb"
+    return f"BAddddddddd!"
+
 if __name__ == "__main__":
     mcp.run(transport="http", port=8000)
